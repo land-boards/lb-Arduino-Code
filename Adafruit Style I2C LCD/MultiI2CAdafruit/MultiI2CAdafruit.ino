90b1be76f84d45f570f7e6e2acd7d1cc8e4d443e
Doug Gilliland <doug@douglasgilliland.com>
2012-04-13 18:59:19 -0400

    GPS added

--- Adafruit Style I2C LCD/MultiI2CAdafruit/MultiI2CAdafruit.ino
+++ Adafruit Style I2C LCD/MultiI2CAdafruit/MultiI2CAdafruit.ino
@@ -1,19 +1,22 @@
 /*
- * Displays text sent over the serial port (e.g. from the Serial Monitor) on
- * an attached LCD.
+ MultiI2CAdafruit - Multi I/O Card using Adadfruit style of LCD.
+ Supported functions are:
+ TMP36A - Temperature sensor
+ MMA7361 - Accelerometer
+ IR Sensor
  */
 
 //////////////////////////////////////////////////////////////
-//
+// Includes follow
 /////////////////////////////////////////////////////////////
 
-// include the library code:
 #include "Wire.h"
 #include "LiquidCrystal.h"
 #include <IRremote.h>
+#include "Ultrasonic.h"
 
 //////////////////////////////////////////////////////////////
-//
+// enums follow
 /////////////////////////////////////////////////////////////
 
 enum IR_VALUES
@@ -42,102 +45,106 @@ enum IR_VALUES
   NINEKEY,
 };
 
-int RECV_PIN = 11;
-
-IRrecv irrecv(RECV_PIN);
-
-decode_results results;
-
-int stateMachineVal;
-
-// Connect via i2c, default address #0 (A0-A2 not jumpered)
-LiquidCrystal lcd(0);
-
-//////////////////////////////////////////////////////////////
-//
-/////////////////////////////////////////////////////////////
-
 enum MENU_STATES
 {
   MENU0 = 0,
+  MENU0B,
+  MENU0C,
   MENU1,
+  MENU1B,
+  MENU1C,
   MENU2,
-  MENU3,
-  MENU4,
+  MENU2B,
+  MENU2C,
   MENU5,
   MENU6,
+  MENU6B,
+  MENU7,
+  MENU7B,
+  MENU7C,
 };
 
 //////////////////////////////////////////////////////////////
-//
+// Global variables follow
+//////////////////////////////////////////////////////////////
+
+int RECV_PIN = 11;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+int stateMachineVal; 
+
+// Connect via i2c, default address #0 (A0-A2 not jumpered)
+
+LiquidCrystal lcd(0);
+
+Ultrasonic ultrasonic(13,4);
+
+short ranges[5];
+
+//////////////////////////////////////////////////////////////
+// setup() -
 /////////////////////////////////////////////////////////////
 
 void setup() {
-  // set up the LCD's number of rows and columns: 
+  // set up the LCD's number of rows and columns:
   lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.setCursor(0,0);
-  lcd.print("*Doug's Arduino*");
-  lcd.setCursor(0,1);
-  lcd.print("Any key to cont.");
   irrecv.enableIRIn(); // Start the receiver
-  stateMachineVal = MENU0;
+  initRanges();
+  stateMachineVal = MENU6;
 }
 
 //////////////////////////////////////////////////////////////
 // loop()
 /////////////////////////////////////////////////////////////
 
-void loop() 
+void loop()
 {
   int irVal;
   irVal = checkIR();
   switch (stateMachineVal)
   {
-  case MENU0:
-    if (irVal != NOKEY)
+  case MENU6:
     {
+      // Print a message to the LCD.
       lcd.setCursor(0,0);
-      lcd.print("> Accelerometer ");
+      lcd.print("*Doug's Arduino*");
       lcd.setCursor(0,1);
-      lcd.print("Temperature     ");
-      if (irVal == RIGHT)
-      {
-        lcd.setCursor(0,0);
-        lcd.print("Accelerometer   ");
-        lcd.setCursor(0,1);
-        lcd.print("> Temperature   ");
-        stateMachineVal = MENU1;
-      }
-      else if (irVal == PAUSE)
-        stateMachineVal = MENU3;
+      lcd.print("Any key to cont.");
+      stateMachineVal = MENU6B;
     }
     break;
-  case MENU1:
-    if (irVal != NOKEY)
+  case MENU6B:
     {
-      if (irVal == RIGHT)
-        stateMachineVal = MENU2;
-      else if (irVal == LEFT)
+      if (irVal != NOKEY)
         stateMachineVal = MENU0;
-      else if (irVal == PAUSE)
-        stateMachineVal = MENU4;
     }
     break;
-  case MENU2:
-    if (irVal == RIGHT)
+  case MENU0:
+    lcd.setCursor(0,0);
+    lcd.print("> Accelerometer ");
+    lcd.setCursor(0,1);
+    lcd.print("Temperature     ");
+    stateMachineVal = MENU0B;
+    break;
+  case MENU0B:
     {
-      lcd.setCursor(0,0);
-      lcd.print("Temperature     ");
-      lcd.setCursor(0,1);
-      lcd.print("> IR Keys       ");
+      if (irVal != NOKEY)
+      {
+        if (irVal == RIGHT)
+        {
+          stateMachineVal = MENU1;
+        }
+        else if (irVal == LEFT)
+        {
+          stateMachineVal = MENU6;
+        }
+        else if (irVal == PAUSE)
+          stateMachineVal = MENU0C;
+      }
     }
-    else if (irVal == PAUSE)
-      stateMachineVal = MENU5;
-    else if (irVal == LEFT)
-      stateMachineVal = MENU1;
-    break;  
-  case MENU3:
+    break;
+  case MENU0C:
     if (irVal != NOKEY)
     {
       stateMachineVal = MENU0;
@@ -145,7 +152,27 @@ void loop()
     else
       accel();
     break;
-  case MENU4:
+  case MENU1:
+    {
+      lcd.setCursor(0,0);
+      lcd.print("Accelerometer   ");
+      lcd.setCursor(0,1);
+      lcd.print("> Temperature   ");
+      stateMachineVal = MENU1B;
+      break;
+    }
+  case MENU1B:
+    if (irVal != NOKEY)
+    {
+      if (irVal == RIGHT)
+        stateMachineVal = MENU2;
+      else if (irVal == LEFT)
+        stateMachineVal = MENU0;
+      else if (irVal == PAUSE)
+        stateMachineVal = MENU1C;
+    }
+    break;
+  case MENU1C:
     if (irVal != NOKEY)
     {
       stateMachineVal = MENU1;
@@ -157,23 +184,83 @@ void loop()
       readTMP36(0);
     }
     break;
+  case MENU2:
+    lcd.setCursor(0,0);
+    lcd.print("Temperature     ");
+    lcd.setCursor(0,1);
+    lcd.print("> IR Keys       ");
+    stateMachineVal = MENU2B;
+    break;
+  case MENU2B:
+    {
+      if (irVal != NOKEY)
+      {
+         if (irVal == LEFT)
+          stateMachineVal = MENU1;
+        else if (irVal == RIGHT)
+          stateMachineVal = MENU7;
+        else if (irVal == PAUSE)
+          stateMachineVal = MENU2C;
+      }
+    }
+    break;
+  case MENU2C:
+    if (irVal != NOKEY)
+    {
+      stateMachineVal = MENU2;
+    }
+    else
+      testIRKeys();
+    break;
   case MENU5:
     if (irVal != NOKEY)
     {
       stateMachineVal = MENU2;
     }
     break;
+  case MENU7:
+    lcd.setCursor(0,0);
+    lcd.print("IR Keys         ");
+    lcd.setCursor(0,1);
+    lcd.print("> Ultrasonic    ");
+    stateMachineVal = MENU7B;
+    break;
+  case MENU7B:
+    {
+      if (irVal != NOKEY)
+      {
+         if (irVal == LEFT)
+          stateMachineVal = MENU2;
+        else if (irVal == PAUSE)
+        {
+          lcd.setCursor(0,0);
+          lcd.print("Ultrasonic range");
+          stateMachineVal = MENU7C;
+        }
+      }
+    }
+    break;
+  case MENU7C:
+    {
+      lcd.setCursor(0,1);
+      putNewRange(ultrasonic.Ranging(INC));
+      lcd.print(getRangeVal());
+      lcd.print(" in            ");
+    }
+    if (irVal != NOKEY)
+      stateMachineVal = MENU7;
+    break;
   }
 }
 
 //////////////////////////////////////////////////////////////
-//
+// checkIR - Check the IR input and convert from the value to the key.
 /////////////////////////////////////////////////////////////
 
 int checkIR()
 {
   int retVal;
-  if (irrecv.decode(&results)) 
+  if (irrecv.decode(&results))
   {
     if (results.value != 0xffffffff)
     {
@@ -230,7 +317,7 @@ int checkIR()
 }
 
 //////////////////////////////////////////////////////////////
-//
+// accel - Display the accelerometer values
 /////////////////////////////////////////////////////////////
 
 void accel(void)
@@ -257,7 +344,7 @@ void accel(void)
   lcd.print(", a=");
   lcd.print(acc);
   lcd.print("      ");
-//  delay(200);
+  //  delay(200);
 }
 
 ///////////////////////////////////////////////////////////////////////
@@ -302,4 +389,52 @@ void readTMP36(int channelNumber)
   //  lcd.println(" deg F");
 }
 
+///////////////////////////////////////////////////////////////////////
+// testIRKeys(
+///////////////////////////////////////////////////////////////////////
+
+void testIRKeys(void)
+{
+  lcd.setCursor(0,0);
+  lcd.print("IR KEY Testing  ");
+  lcd.setCursor(0,1);
+  lcd.print("                ");
+}
+
+void putNewRange(long newRangeVal)
+{
+ ranges[0] = ranges[1];
+ ranges[1] = ranges[2];
+ ranges[2] = ranges[3];
+ ranges[3] = ranges[4];
+ ranges[4] = newRangeVal;
+ return;
+}
+
+void initRanges(void)
+{
+  putNewRange(ultrasonic.Ranging(INC));
+  putNewRange(ultrasonic.Ranging(INC));
+  putNewRange(ultrasonic.Ranging(INC));
+  putNewRange(ultrasonic.Ranging(INC));
+  putNewRange(ultrasonic.Ranging(INC));  
+}
+
+short getRangeVal(void)
+{
+  if (ranges[4] == ranges[3])
+    return(ranges[4]);
+  else if (ranges[4] == ranges[3] + 1)
+    return(ranges[3]);
+  else if (ranges[4] == ranges[3] -1 )
+    return(ranges[3]);
+  else if (ranges[4] == ranges[2])
+    return(ranges[4]);
+  else if (ranges[3] == ranges[2])
+    return(ranges[3]);
+  else if (ranges[2] == ranges[1])
+    return(ranges[2]);
+  else return(ranges[4]);
+  
+}
 
