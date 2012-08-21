ee42964093bf9ee6700feca2fc1f540578e2a90a
Doug Gilliland <doug@douglasgilliland.com>
2012-08-21 06:54:17 -0400

    fan code updates

---
+++ Sensors/tinyFan/DS18S20.ino
@@ -0,0 +1,88 @@
+#include <OneWire.h>
+#include <Arduino.h>
+
+// OneWire DS18S20, DS18B20, DS1822 Temperature Example
+//
+// http://www.pjrc.com/teensy/td_libs_OneWire.html
+//
+// The DallasTemperature library can do all this work for you!
+// http://milesburton.com/Dallas_Temperature_Control_Library
+
+OneWire  ds(4);  // on pin 
+
+int sensNum;
+float celsius, fahrenheit;
+
+void read1Wire(void) 
+{
+  byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+
+  //  setCursorTFT(1,0);
+  if ( !ds.search(addr)) {
+    //    tft.println("No more addresses.");
+    ds.reset_search();
+    delay(250);
+    sensNum = 0;
+    return;
+  }
+
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+    return;
+  }
+
+  switch (addr[0]) {
+  case 0x10:
+    type_s = 1;
+    break;
+  case 0x28:
+    type_s = 0;
+    break;
+  case 0x22:
+    type_s = 0;
+    break;
+  default:
+    return;
+  } 
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44,1);         // start conversion, with parasite power on at the end
+
+  delay(1000);     // maybe 750ms is enough, maybe not
+  // we might do a ds.depower() here, but the reset will take care of it.
+
+  present = ds.reset();
+  ds.select(addr);    
+  ds.write(0xBE);         // Read Scratchpad
+
+  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = ds.read();
+  }
+
+  unsigned int raw = (data[1] << 8) | data[0];
+  if (type_s) 
+  {
+    raw = raw << 3; // 9 bit resolution default
+    if (data[7] == 0x10) 
+    {
+      // count remain gives full 12 bit resolution
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  }
+  else 
+  {
+    byte cfg = (data[4] & 0x60);
+    if (cfg == 0x00) raw = raw << 3;  // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20) raw = raw << 2; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40) raw = raw << 1; // 11 bit res, 375 ms
+    // default is 12 bit resolution, 750 ms conversion time
+  }
+  celsius = (float)raw / 16.0;
+  fahrenheit = celsius * 1.8 + 32.0;
+  sensNum++;
+}
+
--- Sensors/tinyFan/tinyFan.ino
+++ Sensors/tinyFan/tinyFan.ino
@@ -1,10 +1,10 @@
 /////////////////////////////////////////////////////////////////////
-// roomFan - Control an inlet fan.
+// roomFan - Control an inlet fan using an ATtiny85.
 // Hardware Inputs:
-//   Analog 0 - LDR.
-//   Digital 6 - 1-wire temp sensor.
+//   "Pin" 2 - LDR.
+//   "Pin" 4 - 1-wire temp sensor.
 // Hardware Outputs:
-//   Digital 3 - Relay output.
+//   "Pin" 3 - Relay output.
 /////////////////////////////////////////////////////////////////////
 
 #include <Time.h>
@@ -13,10 +13,8 @@
 // defines
 /////////////////////////////////////////////////////////////////////
 
-//#define DEBUG 1
-#undef DEBUG
-
-#define RELAY_PIN 2
+#define RELAY_PIN 3
+#define LDR_PIN 2
 #define DARK_THR 500
 #define LOW_TEMP 62.0
 #define DARK_HYST 20
@@ -51,15 +49,11 @@ int useLDR;
 // the setup routine runs once when you press reset:
 void setup() 
 {
-  // initialize serial communication at 9600 bits per second:
-#ifdef DEBUG
-  Serial.begin(9600);
-#endif
   pinMode(RELAY_PIN, OUTPUT);
   fanState = FAN_OFF;
   setFan(fanState);
   useLDR = DARK_THR;
-  todaysLDRHigh = analogRead(A1);
+  todaysLDRHigh = analogRead(LDR_PIN);
   todaysLDRLow = todaysLDRHigh;
 }
 
@@ -82,57 +76,26 @@ void setFan(int fanVal)
 void loop() 
 {
   // read the input on analog pin 1
-  int sensorValue = analogRead(A1);
-  // print out the value you read:
-#ifdef DEBUG
-  Serial.print("LDR ");
-  Serial.println(sensorValue);
-#endif
-
+  int sensorValue = analogRead(LDR_PIN);
   if (sensorValue > todaysLDRHigh)
-  {
     todaysLDRHigh = sensorValue;
-  }
   else if (sensorValue < todaysLDRLow)
-  {
     todaysLDRLow = sensorValue;
-  }
 
   if ((fanState == FAN_OFF) && (sensorValue > useLDR + DARK_HYST) && (fahrenheit > LOW_TEMP))
   {
     fanState = FAN_ON;
-#ifdef DEBUG
-    Serial.println("Turning fan on");
-#endif
     setFan(FAN_ON);
   }
   else if ((fanState == FAN_ON) && (sensorValue < useLDR - DARK_HYST))
   {
     fanState = FAN_OFF;
-#ifdef DEBUG
-    Serial.println("Turning fan off");
-#endif
     setFan(FAN_OFF);
   }
   delay(1500);        // delay in between reads for stability
   read1Wire();
-#ifdef DEBUG
-  Serial.print("Temp ");
-  Serial.println(fahrenheit);
-  digitalClockDisplay();
-#endif
   if (day() > 1)
   {
-#ifdef DEBUG
-    Serial.println("crossed 1 day");
-    Serial.println("reset time");
-    Serial.print("todaysLDRHigh = ");
-    Serial.println(todaysLDRHigh);
-    Serial.print("todaysLDRLow = ");
-    Serial.println(todaysLDRLow);
-    Serial.print("useLDR = ");
-    Serial.println(useLDR);
-#endif
     setTime(0);
     yesterdaysLDRHigh = todaysLDRHigh;
     yesterdaysLDRLow = todaysLDRLow;
@@ -141,37 +104,4 @@ void loop()
     useLDR = (yesterdaysLDRHigh + yesterdaysLDRLow) / 2;
   }
 }
-
-/////////////////////////////////////////////////////////////////////
-// digitalClockDisplay()
-/////////////////////////////////////////////////////////////////////
-
-void digitalClockDisplay()
-{
-  // digital clock display of the time
-  Serial.print(hour());
-  printDigits(minute());
-  printDigits(second());
-  Serial.print(" ");
-  Serial.print(day());
-  Serial.print(" ");
-  Serial.print(month());
-  Serial.print(" ");
-  Serial.print(year());
-  Serial.println();
-}
-
-/////////////////////////////////////////////////////////////////////
-// printDigits(int digits)
-/////////////////////////////////////////////////////////////////////
-
-void printDigits(int digits)
-{
-  // utility function for digital clock display: prints preceding colon and leading 0
-  Serial.print(":");
-  if(digits < 10)
-    Serial.print('0');
-  Serial.print(digits);
-}
-
 
