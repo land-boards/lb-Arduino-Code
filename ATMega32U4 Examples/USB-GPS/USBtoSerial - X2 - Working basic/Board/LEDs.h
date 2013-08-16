68aeae122d9747c4287ec0b2950b8bb33fdda4e3
douggilliland <doug@douglasgilliland.com>
2013-08-16 10:37:49 -0400

    Did stuff with the Hammond X2 GPS board

---
+++ ATMega32U4 Examples/USB-GPS/USBtoSerial - X2 - Working basic/Board/LEDs.h
@@ -0,0 +1,56 @@
+/*
+             LEDs Library
+     Copyright (C) Douglas Gilliland, 2012.
+
+  doug [at] douglasgilliland [dot] com
+           www.douglasgilliland.com
+*/
+
+/*
+  Copyright 2012  Douglas Gilliland (doug [at] douglasgilliland [dot] com)
+
+  Permission to use, copy, modify, distribute, and sell this
+  software and its documentation for any purpose is hereby granted
+  without fee, provided that the above copyright notice appear in
+  all copies and that both that the copyright notice and this
+  permission notice and warranty disclaimer appear in supporting
+  documentation, and that the name of the author not be used in
+  advertising or publicity pertaining to distribution of the
+  software without specific, written prior permission.
+
+  The author disclaim all warranties with regard to this
+  software, including all implied warranties of merchantability
+  and fitness.  In no event shall the author be liable for any
+  special, indirect or consequential damages or any damages
+  whatsoever resulting from loss of use, data or profits, whether
+  in an action of contract, negligence or other tortious action,
+  arising out of or in connection with the use or performance of
+  this software.
+*/
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <inttypes.h>
+#include "USBtoSerial.h"
+
+void LEDs_SetAllLEDs(int LEDVal)
+{
+	if (LEDVal & 0x1)
+		PORTB |= 0x80;
+	else
+		PORTB &= 0x7f;
+	if (LEDVal & 0x2)
+		PORTD |= 0x01;
+	else
+		PORTD &= 0xfe;
+}
+
+void LEDs_Init(void)
+{
+	DDRB = 0x80;
+	PORTB &= 0x7f;
+  DDRD = 0x01;
+  PORTD &= 0xfe
+}
+
+
--- Blink85/Blink85.ino
+++ Blink85/Blink85.ino
@@ -1,19 +1,22 @@
 /*
-  Blink
+  Based on Blink from the Arduino sketch
   Turns on an LED on for one second, then off for one second, repeatedly.
  
   This example code is in the public domain.
  */
 
+#define LED_PORT 2
+
 void setup() {                
   // initialize the digital pin as an output.
   // Pin 13 has an LED connected on most Arduino boards:
-  pinMode(4, OUTPUT);     
+  pinMode(LED_PORT, OUTPUT);     
 }
 
-void loop() {
-  digitalWrite(4, HIGH);   // set the LED on
-  delay(1000);              // wait for a second
-  digitalWrite(4, LOW);    // set the LED off
-  delay(1000);              // wait for a second
+void loop() 
+{
+  digitalWrite(LED_PORT, HIGH);   // set the LED on
+  delay(1000);                    // wait for a second
+  digitalWrite(LED_PORT, LOW);    // set the LED off
+  delay(1000);                    // wait for a second
 }
