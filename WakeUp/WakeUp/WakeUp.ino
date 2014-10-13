5409c11142d8ffdbf1b018f450236940ea0d5023
Doug Gilliland <doug@douglasgilliland.com>
2014-10-12 22:40:35 -0400

    Made WakeUp work

--- WakeUp/WakeUp/WakeUp.ino
+++ WakeUp/WakeUp/WakeUp.ino
@@ -1,31 +1,23 @@
-// Sample Sketch for TinyGrid85
-// Bounces LED from one pin to another
+// WakeUp
 
-// LED pins on 0-4
-int LED0 = 0;
-int LED1 = 1;
-int LED2 = 2;
-int LED3 = 3;
-int LED4 = 4;
-const int buttonPin = 3;
-int ledNum; // the current LED that is lighted
-int sensorValue;
+// Hardware
+const int BUZZER = 0;
+const int BUTTON = 3;
 
 void setup() 
 {
-  pinMode(LED0, OUTPUT);
-  pinMode(buttonPin, INPUT);  
+  pinMode(BUZZER, OUTPUT);
+  pinMode(BUTTON, INPUT);
 }
 
 void loop() 
 {
-  // a very simple switch statement
-
-  int switchA = 3;
+  int sensorValue;
+  
   sensorValue = analogRead(2);
   if (sensorValue > 512)
-    digitalWrite(LED0, LOW);
-   else
-    digitalWrite(LED0, HIGH);
+    digitalWrite(BUZZER, LOW);
+  else
+    digitalWrite(BUZZER, HIGH);
 }
 
