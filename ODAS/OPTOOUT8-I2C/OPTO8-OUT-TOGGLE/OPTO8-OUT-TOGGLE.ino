2405375a9ebb9b650204f999a18b738e6083b6b2
Doug Gilliland <doug@douglasgilliland.com>
2015-05-17 16:48:50 -0400

    OPTOOUT8 TESTS

--- ODAS/OPTOOUT8-I2C/OPTO8-OUT-TOGGLE/OPTO8-OUT-TOGGLE.ino
+++ ODAS/OPTOOUT8-I2C/OPTO8-OUT-TOGGLE/OPTO8-OUT-TOGGLE.ino
@@ -5,30 +5,56 @@
 
 Adafruit_MCP23008 mcp;
 
+unsigned long testCount;
+unsigned long passCount;
+unsigned long failCount;
+
 void setup()
 {
   int loopCnt;
+  Serial.begin(57600);
   mcp.begin();      // use default address 0
   TWBR = 12;      // go to 400 KHz I2C speed mode
   for (loopCnt = 0; loopCnt < 8; loopCnt++)
     mcp.pinMode(loopCnt, OUTPUT);
   for (loopCnt = 4; loopCnt < 12; loopCnt++)
-    pinMode(loopCnt, INPUT_PULLUP);
+    pinMode(loopCnt, INPUT);
+  testCount = 0;
+  passCount = 0;
+  failCount = 0;
+  
 }
 
-// flip the pin #0 up and down
+// 
 
 void loop()
 {
   int loopCnt;
+  unsigned char digRdVal;
+  int pass1Fail0;
   delay(100);
-
+  testCount++;
+  pass1Fail0 = 1;
   for (loopCnt = 0; loopCnt < 8; loopCnt++)
   {
     mcp.digitalWrite(loopCnt, HIGH);
-    Serial.println(digitalRead(loopCnt+4));
+    delayMicroseconds(15);
+    digRdVal = digitalRead(loopCnt+4);
+    if (digRdVal != HIGH)
+      pass1Fail0 = 0;
     mcp.digitalWrite(loopCnt, LOW);
-    Serial.println(digitalRead(loopCnt+4));
+    delayMicroseconds(15);
+    digRdVal = digitalRead(loopCnt+4);
+    if (digRdVal != LOW)
+      pass1Fail0 = 0;
   }
+  if (pass1Fail0 == 1)
+    passCount++;
+   else
+     failCount++;
+   Serial.print("Pass Count = ");
+   Serial.print(passCount);
+   Serial.print(", Fail Count = ");
+   Serial.println(failCount);
 }
 
