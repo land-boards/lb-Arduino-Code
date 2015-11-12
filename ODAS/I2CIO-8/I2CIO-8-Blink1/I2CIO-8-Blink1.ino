69cc38424d872e094c8700cec4104688d6759724
Doug Gilliland <doug@douglasgilliland.com>
2015-11-11 21:17:21 -0500

    Blink for I2CIO-8 card

---
+++ ODAS/I2CIO-8/I2CIO-8-Blink1/I2CIO-8-Blink1.ino
@@ -0,0 +1,25 @@
+//
+
+#include <Wire.h>
+#include "Adafruit_MCP23008.h"
+
+Adafruit_MCP23008 mcp;
+
+void setup()
+{
+  mcp.begin();      // use default address 0
+//  TWBR = 12;      // go to 400 KHz I2C speed mode
+  for (int loopCnt = 0; loopCnt < 4; loopCnt++)
+    mcp.pinMode(loopCnt, OUTPUT);
+}
+
+//
+
+void loop()
+{
+  mcp.digitalWrite(0, HIGH);
+  delay(500);
+  mcp.digitalWrite(0, LOW);
+  delay(500);
+}
+
