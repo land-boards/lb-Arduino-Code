0a04bcafce0ccd738e3adcb9d7af09213cd30605
Doug Gilliland <doug@douglasgilliland.com>
2016-07-18 22:48:33 -0400

    wemos blinky

---
+++ WeMos-Blink/WeMos-Blink.ino
@@ -0,0 +1,16 @@
+/*
+ * Blink
+ * Turns on the onboard LED on for one second, then off for one second, repeatedly.
+ * This uses delay() to pause between LED toggles.
+ */
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);  // initialize onboard LED as output
+}
+
+void loop() {
+  digitalWrite(BUILTIN_LED, HIGH);  // turn on LED with voltage HIGH
+  delay(2000);                      // wait one second
+  digitalWrite(BUILTIN_LED, LOW);   // turn off LED with voltage LOW
+  delay(1000);                      // wait one second
+}
