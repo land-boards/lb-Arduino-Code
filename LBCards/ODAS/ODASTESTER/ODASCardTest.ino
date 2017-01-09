ee931374c4cd14a2179a13ebacfe2cf44e056c43
Doug Gilliland <doug@douglasgilliland.com>
2017-01-08 19:07:37 -0500

    move out bounceLED code

--- LBCards/ODAS/ODASTESTER/ODASCardTest.ino
+++ LBCards/ODAS/ODASTESTER/ODASCardTest.ino
@@ -1,29 +1,10 @@
-void bounceLedsCard(void)
-{
-  switch (boardType)
-  {
-    case DIGIO16I2C:
-      break;
-    case DIGIO128:
-      bounceLedsDigio128();
-      break;
-    case OPTOIN8I2C:
-      Serial.println("Can't bounce LEDs on an input only card");
-      break;
-    case OPTOOUT8I2C:
-      break;
-    case DIGIO32I2C:
-      bounceLedsDigio32();
-      break;
-    case PROTO16I2C:
-      break;
-    case ODASPSOC5:
-      break;
-    case NEWBOARD:
-      break;
-  }
-}
+//////////////////////////////////////////////////////////////////////////////////////
+// ODAS card loopback test code
+//////////////////////////////////////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////////////////////////////////////
+// uint8_t loopBackTestCard(void) - 
+//////////////////////////////////////////////////////////////////////////////////////
 
 uint8_t loopBackTestCard(void)
 {
@@ -56,66 +37,6 @@ uint8_t loopBackTestCard(void)
   }
   return 1; // fail
 }
-
-//////////////////////////////////////////////////////////////////////////////////////
-//
-//////////////////////////////////////////////////////////////////////////////////////
-
-void bounceLedsDigio32(void)
-{
-  Digio32 Dio32;
-  Dio32.begin(0);
-  Serial.println("Bouncing LEDs - ant key to stop");
-  while (1)
-  {
-    for (uint8_t port = 0; port < 32; port++)
-    {
-      Dio32.pinMode(port, OUTPUT);
-      Dio32.digitalWrite(port, HIGH);
-      delay(2);
-      Dio32.digitalWrite(port, LOW);
-      delay(2);
-    }
-    if (Serial.available() > 0)
-    {
-      Serial.read();
-      return;
-    }
-  }
-}
-
-//////////////////////////////////////////////////////////////////////////////////////
-// void bounceLedsDigio128(void)
-//////////////////////////////////////////////////////////////////////////////////////
-
-void bounceLedsDigio128(void)
-{
-  Digio128 Dio128;
-  Dio128.begin();
-  Serial.println("Bouncing LEDs DIGIO-128 - any key to stop");
-  while (1)
-  {
-    for (uint8_t port = 0; port < 127; port++)
-    {
-      Dio128.pinMode(port, OUTPUT);
-      delay(20);
-      Dio128.digitalWrite(port, HIGH);
-      delay(20);
-      Dio128.digitalWrite(port, LOW);
-      delay(20);
-    }
-    if (Serial.available() > 0)
-    {
-      Serial.read();
-      return;
-    }
-  }
-}
-
-
-//////////////////////////////////////////////////////////////////////////////////////
-// Digio32-I2C card test code
-//////////////////////////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////////////////////////
 // uint8_t loopBackTestDigio32()
