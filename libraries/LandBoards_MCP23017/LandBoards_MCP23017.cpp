851cfdda031ace32f1e13be853567f810c99b149
Doug Gilliland <doug@douglasgilliland.com>
2019-03-03 15:25:09 -0500

    Create MCP23017 library
    Reduce dependency on other libraries

---
+++ libraries/LandBoards_MCP23017/LandBoards_MCP23017.cpp
@@ -0,0 +1,193 @@
+////////////////////////////////////////////////////////////////////////////
+//  LandBoards_MCP23017.cpp - Library for MCP23017
+//  Created by Douglas Gilliland. 2019-03-03
+//	Base address of the part is set one of eight addresses
+//	2019-03-03 - Added support for STM32F1 "blus pill" boards
+////////////////////////////////////////////////////////////////////////////
+//	Library class supports multiple types of access:
+//		Arduino-ish (bit) oriented
+//      Byte oriented
+//      I2C Low Level Driver (I2C register access)oriented
+////////////////////////////////////////////////////////////////////////////
+
+#include <Wire.h>
+#include <Arduino.h>
+#include <inttypes.h>
+
+#include "LandBoards_MCP23017.h"
+
+////////////////////////////////////////////////////////////////////////////
+// LandBoards_MCP23017 constructor - has no address since the card uses all 0x20-0x27
+////////////////////////////////////////////////////////////////////////////
+
+LandBoards_MCP23017::LandBoards_MCP23017(void)
+{
+	return;
+}
+
+////////////////////////////////////////////////////////////////////////////
+// begin(baseAddr) - Initialize the part
+// Sets all bits to inputs
+// Sets the global baseAddress - board base address register
+////////////////////////////////////////////////////////////////////////////
+
+void LandBoards_MCP23017::begin(uint8_t baseAddr)
+{
+	boardBaseAddr = MCP23017_ADDRESS + (baseAddr&7);	// baseAddr set by jumpers
+	Wire.begin();
+#if defined(ARDUINO_ARCH_AVR)
+	TWBR = 12;    			// go to 400 KHz I2C speed mode
+#elif defined(ARDUINO_ARCH_STM32F1)
+	Wire.setClock(400000);	// 400KHz speed
+#else
+  #error “This library only supports boards with an AVR or SAM processor.”
+#endif
+	writeRegister(MCP23017_IODIRA, 0xff);		// bits are all inputs
+	writeRegister(MCP23017_GPPUA, 0x00);		// Turn off pullups
+	writeRegister(MCP23017_IODIRB, 0xff);		// bits are all inputs
+	writeRegister(MCP23017_GPPUB, 0x00);		// Turn off pullups
+	return;
+}
+
+////////////////////////////////////////////////////////////////////////////
+// void digitalWrite(uint8_t bit, uint8_t wrVal) - write to a bit
+////////////////////////////////////////////////////////////////////////////
+
+void LandBoards_MCP23017::digitalWrite(uint8_t bit, uint8_t wrVal)
+{
+	uint8_t regAdr;
+	uint8_t rdVal;
+	if ((bit & 0x08) == 0)
+		regAdr = MCP23017_OLATA;
+	else
+		regAdr = MCP23017_OLATB;
+	rdVal = readRegister(regAdr);
+	if (wrVal == 0)
+		rdVal &= ~(1 << (bit&0x7));
+	else
+		rdVal |= (1 << (bit&0x7));
+	writeRegister(regAdr, rdVal);
+}
+
+////////////////////////////////////////////////////////////////////////////
+// uint8_t digitalRead(uint8_t p) - read back a bit
+////////////////////////////////////////////////////////////////////////////
+
+uint8_t LandBoards_MCP23017::digitalRead(uint8_t bit)
+{
+	uint8_t regAdr;
+	uint8_t rdVal;
+	if ((bit & 0x08) == 0)
+		regAdr = MCP23017_GPIOA;
+	else
+		regAdr = MCP23017_GPIOB;
+	rdVal = readRegister(regAdr);
+	return ((rdVal>>(bit&7))&0x01);
+}
+
+////////////////////////////////////////////////////////////////////////////
+// void PinMode(port, direction)- Set the direction of a bit (INPUT, OUTPUT, INPUT_PULLUP)
+// Arduino.h defines for direction are:
+//	#define INPUT 0x0
+//	#define OUTPUT 0x1
+//	#define INPUT_PULLUP 0x2
+// This function only changes the direction of one bit at a time
+// This function affects the pull-up and direction registers in the MCP23017
+// Pull-up bit - 1=use pull-up on input, 0=don't use pull-up
+// Direction bit - 1=Input, 0=Output
+////////////////////////////////////////////////////////////////////////////
+
+void LandBoards_MCP23017::pinMode(uint8_t bit, uint8_t value)
+{
+	uint8_t puRegAdr;	// Pull-up register address
+	uint8_t dirRegAdr;	// Direction register address
+	uint8_t rdPuVal;	// Value of the pull-up register
+	uint8_t rdDirVal;	// Value of the direction register
+	uint8_t changeBit;	// The bit that changes in the register
+	changeBit = 1 << (bit & 0x7);
+	if ((bit & 0x08) == 0)	// A registers
+	{
+		puRegAdr = MCP23017_GPPUA;
+		dirRegAdr = MCP23017_IODIRA;
+	}
+	else					// B registers
+	{
+		puRegAdr = MCP23017_GPPUB;
+		dirRegAdr = MCP23017_IODIRB;
+	}
+	rdPuVal = readRegister(puRegAdr);		// Read the previous pull-up reg value
+	rdDirVal = readRegister(dirRegAdr);	// Read the previous direction reg value
+	if (value == INPUT_PULLUP)		// 
+	{
+		rdPuVal  |= changeBit;
+		writeRegister(puRegAdr,rdPuVal);
+		rdDirVal |= changeBit;
+		writeRegister(dirRegAdr,rdDirVal);
+	}
+	else if (value == INPUT)
+	{
+		rdPuVal &= ~changeBit;
+		writeRegister(puRegAdr,rdPuVal);
+		rdDirVal |= changeBit;
+		writeRegister(dirRegAdr,rdDirVal);
+	}
+	else if (value == OUTPUT)
+	{
+		rdDirVal &= ~changeBit;
+		writeRegister(dirRegAdr,rdDirVal);
+	}
+} 
+
+////////////////////////////////////////////////////////////////////////////
+// void writeOLATAB(baData) - Write 16-bits of data at a time
+// There are two chips on the card
+////////////////////////////////////////////////////////////////////////////
+
+void LandBoards_MCP23017::writeOLATAB(uint16_t baData)
+{
+	writeRegister(MCP23017_OLATB,(baData>>8)&0xff);
+	writeRegister(MCP23017_OLATA,baData&0xff);
+}
+
+////////////////////////////////////////////////////////////////////////////
+// uint16_t readGPIOAB(chip) - Read 16-bits at a time
+////////////////////////////////////////////////////////////////////////////
+
+uint16_t LandBoards_MCP23017::readGPIOAB()
+{
+	return (((readRegister(MCP23017_GPIOB)<<8)&0xff)|(readRegister(MCP23017_GPIOA)&0xff));
+}
+
+////////////////////////////////////////////////////////////////////////////
+// uint16_t readOLATAB(chip) - Read 16-bits at a time
+////////////////////////////////////////////////////////////////////////////
+
+uint16_t LandBoards_MCP23017::readOLATAB()
+{
+	return (((readRegister(MCP23017_OLATB)<<8)&0xff00)|(readRegister(MCP23017_OLATA)&0xff));
+}
+
+////////////////////////////////////////////////////////////////////////////
+// uint8_t LandBoards_MCP23017::readRegister(uint8_t  uint8_t regAddr)
+////////////////////////////////////////////////////////////////////////////
+
+uint8_t LandBoards_MCP23017::readRegister(uint8_t regAddr)
+{
+	Wire.beginTransmission(boardBaseAddr);
+	Wire.write(regAddr);
+	Wire.endTransmission();
+	Wire.requestFrom(boardBaseAddr,  1);
+	return Wire.read();
+}
+	
+////////////////////////////////////////////////////////////////////////////
+// void LandBoards_MCP23017::writeRegister(uint8_t  uint8_t regAddr, uint8_t value)
+////////////////////////////////////////////////////////////////////////////
+
+void LandBoards_MCP23017::writeRegister(uint8_t regAddr, uint8_t value)
+{
+	Wire.beginTransmission(boardBaseAddr);
+	Wire.write(regAddr);
+	Wire.write(value);
+	Wire.endTransmission(1);
+}
---
+++ libraries/LandBoards_MCP23017/LandBoards_MCP23017.h
@@ -0,0 +1,73 @@
+#ifndef LandBoards_MCP23017_h
+#define LandBoards_MCP23017_h
+
+////////////////////////////////////////////////////////////////////////////
+//  LandBoards_MCP23017 - Library for Land Boards MCP23017 chip
+//	Communication with the card is via I2C Two-wire interface
+//  This library allows for both bit access and chip access to the card
+//		Chip access (16-bits) via writeGPIOAB, readGPIOAB
+////////////////////////////////////////////////////////////////////////////
+
+#include <inttypes.h>
+#include "Arduino.h"
+#include <Wire.h>
+
+#define MCP23017_ADDRESS 0x20
+
+// registers
+#define MCP23017_IODIRA 0x00
+#define MCP23017_IPOLA 0x02
+#define MCP23017_GPINTENA 0x04
+#define MCP23017_DEFVALA 0x06
+#define MCP23017_INTCONA 0x08
+#define MCP23017_IOCONA 0x0A
+#define MCP23017_GPPUA 0x0C
+#define MCP23017_INTFA 0x0E
+#define MCP23017_INTCAPA 0x10
+#define MCP23017_GPIOA 0x12
+#define MCP23017_OLATA 0x14
+
+
+#define MCP23017_IODIRB 0x01
+#define MCP23017_IPOLB 0x03
+#define MCP23017_GPINTENB 0x05
+#define MCP23017_DEFVALB 0x07
+#define MCP23017_INTCONB 0x09
+#define MCP23017_IOCONB 0x0B
+#define MCP23017_GPPUB 0x0D
+#define MCP23017_INTFB 0x0F
+#define MCP23017_INTCAPB 0x11
+#define MCP23017_GPIOB 0x13
+#define MCP23017_OLATB 0x15
+
+#define CHIP_SHIFT 0x04
+#define CHIP_MASK 0x10
+#define CHIP_COUNT 2
+
+////////////////////////////////////////////////////////////////////////////////////
+// enums follow
+////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////
+// I2C Ports
+//	MCP23017 0x20-0x21 (or any offset of 2)
+////////////////////////////////////////////////////////////////////////////
+
+class LandBoards_MCP23017
+{
+  public:
+    LandBoards_MCP23017(void);
+	void begin(uint8_t);					// Set the base address
+	void digitalWrite(uint8_t,uint8_t);		// Writes to a single bit
+	uint8_t digitalRead(uint8_t);			// Reads a single bit
+	void pinMode(uint8_t,uint8_t);			// Set the single bit direction (INPUT, INPUT_PULLUP, OUTPUT)
+	void writeOLATAB(uint16_t);		// Write to a 16-bit chip
+	uint16_t readGPIOAB();			// Read from a 16-bit chip
+	uint16_t readOLATAB();			// Read from a 16-bit chip
+  private:
+	uint8_t boardBaseAddr;
+	uint8_t readRegister(uint8_t addr);
+	void writeRegister(uint8_t addr, uint8_t value);
+};
+
+#endif
