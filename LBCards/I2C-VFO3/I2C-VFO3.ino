2616260a5e42ece71331783a180e54f14674004e
Doug Gilliland <doug@douglasgilliland.com>
2021-03-10 10:37:53 -0500

    I2C-VFO3 code

---
+++ LBCards/I2C-VFO3/I2C-VFO3.ino
@@ -0,0 +1,152 @@
+/*
+
+  I2C_VFO3.ino
+
+  http://land-boards.com/blwiki/index.php?title=HW-9_VFO
+
+  OLED is 128x32 I2C device
+  Si5351 is on a custom board
+  Runs on MiniDuino so no 5V to 3.3V level shifting is needed
+
+  Borrowed bits pieces from all over the place
+  Si6361 driver is from si5351_example
+  https://etherkit.github.io/si5351abb_landing_page.html
+  Copyright (C) 2015 - 2016 Jason Milldrum <milldrum@gmail.com>
+  
+*/
+
+#include <Arduino.h>
+#include <EEPROM.h>
+#include <U8g2lib.h>
+#include "si5351.h"
+
+// EEPROM is used to store initial values
+// STM3232 parts have no internal EEPROM
+// ATMEGA parts have internal EEPROM
+// Set defines/undefs appropriately
+#define HAS_INTERNAL_EEPROM
+//#undef HAS_INTERNAL_EEPROM
+
+#define encoderSwitch  4
+
+#define U8X8_HAVE_HW_I2C
+
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+Si5351 si5351;
+
+// enum si5351_drive {SI5351_DRIVE_2MA, SI5351_DRIVE_4MA, SI5351_DRIVE_6MA, SI5351_DRIVE_8MA};
+
+si5351_drive VFO_0_Drive;
+si5351_drive VFO_1_Drive;
+si5351_drive VFO_2_Drive;
+
+enum VFO_ON_OFF
+{
+  VFO_OFF,
+  VFO_ON
+};
+
+unsigned long VFO_0_Freq;
+unsigned long VFO_1_Freq;
+unsigned long VFO_2_Freq;
+
+uint8_t VFO_O_OnOff;
+uint8_t VFO_1_OnOff;
+uint8_t VFO_2_OnOff;
+
+uint8_t currentVFONumber;
+int32_t calFactor;
+
+#define STEP_1_HZ     100ULL
+#define STEP_10_HZ    1000ULL
+#define STEP_100_HZ   10000ULL
+#define STEP_1_KHZ    100000ULL
+#define STEP_10_KHZ   1000000ULL
+#define STEP_100_KHZ  10000000ULL
+#define STEP_1_MHZ    100000000ULL
+#define STEP_10_MHZ   1000000000ULL
+
+// Gloibal variables
+uint32_t stepSize;
+
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);  // STM32, Ebay OLED
+
+// Leave other SSD1306 constructors just in case
+//U8G2_SSD1306_128X32_WINSTAR_F_HW_I2C u8g2(U8G2_R0,  U8X8_PIN_NONE);   // STM32
+//U8G2_SSD1306_128X32_WINSTAR_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ SCL, /* data=*/ SDA);   // pin remapping with ESP8266 HW I2C
+//U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);  // Adafruit ESP8266/32u4/ARM Boards + FeatherWing OLED
+//U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ SCL, /* data=*/ SDA);   // pin remapping with ESP8266 HW I2C
+//U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* SCL=PB6*/ PB6, /* SDA=PB7=*/ PB7, /* reset=*/ U8X8_PIN_NONE);
+//U8G2_SSD1306_64X32_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+//U8G2_SSD1306_64X32_1F_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+//U8G2_SSD1306_96X16_ER_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);   // EastRising 0.69" OLED
+//U8G2_SSD1306_72X40_ER_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);   // EastRising 0.42" OLED
+//U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+//U8G2_NULL u8g2(U8G2_R0);	// null device, a 8x8 pixel display which does nothing
+//U8G2_SSD1306_128X64_NONAME_F_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 12, /* dc=*/ 4, /* reset=*/ 6);	// Arduboy (Production, Kickstarter Edition)
+//U8G2_SSD1306_128X64_NONAME_F_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);
+//U8G2_SSD1306_128X64_NONAME_F_3W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* reset=*/ 8);
+//U8G2_SSD1306_128X64_NONAME_F_3W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 10, /* reset=*/ 8);
+//U8G2_SSD1306_128X64_ALT0_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);   // same as the NONAME variant, but may solve the "every 2nd line skipped" problem
+//U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* reset=*/ 8);
+//U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // All Boards without Reset of the Display
+//U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 16, /* data=*/ 17, /* reset=*/ U8X8_PIN_NONE);   // ESP32 Thing, pure SW emulated I2C
+//U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ 16, /* data=*/ 17);   // ESP32 Thing, HW I2C with pin remapping
+//U8G2_SSD1306_128X64_NONAME_F_6800 u8g2(U8G2_R0, 13, 11, 2, 3, 4, 5, 6, A4, /*enable=*/ 7, /*cs=*/ 10, /*dc=*/ 9, /*reset=*/ 8);
+//U8G2_SSD1306_128X64_NONAME_F_8080 u8g2(U8G2_R0, 13, 11, 2, 3, 4, 5, 6, A4, /*enable=*/ 7, /*cs=*/ 10, /*dc=*/ 9, /*reset=*/ 8);
+//U8G2_SSD1306_128X64_VCOMH0_F_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);	// same as the NONAME variant, but maximizes setContrast() range
+//U8G2_SSD1306_64X48_ER_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);   // EastRising 0.66" OLED breakout board, Uno: A4=SDA, A5=SCL, 5V powered
+//U8G2_SSD1306_48X64_WINSTAR_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+// End of constructor list
+
+
+void setup(void)
+{
+  bool i2c_found;
+
+  checkEEPROM();
+  
+  u8g2.begin();
+  
+  Serial.begin(9600);
+
+  // Si8351 initialiation
+  i2c_found = si5351.init(SI5351_CRYSTAL_LOAD_10PF, 0, 0);
+  if (!i2c_found)
+  {
+    Serial.println("Device not found on I2C bus!");
+  }
+
+  // Set calibration factor
+  si5351.set_correction(calFactor, SI5351_PLL_INPUT_XO);
+  
+  // Set CLK0 to output 14 MHz
+  si5351.set_freq(VFO_0_Freq, SI5351_CLK0);
+
+  // Set CLK1 to output 12 MHz
+  si5351.set_freq(VFO_1_Freq, SI5351_CLK1);
+
+  // Set CLK2 to output 10 MHz
+  si5351.set_freq(VFO_2_Freq, SI5351_CLK2);
+
+  // Query a status update and wait a bit to let the Si5351 populate the
+  // status flags correctly.
+  si5351.update_status();
+  
+  // Encoder initialization
+  pinMode(encoderSwitch, INPUT_PULLUP);
+  setupEncoder();
+  delay(100);
+}
+
+void loop(void)
+{
+  while (1)
+  {
+      vfoMenu();
+  }
+}
---
+++ LBCards/I2C-VFO3/vfoMenu.ino
@@ -0,0 +1,533 @@
+// Menu System for the VFO
+
+enum MenuStateValues
+{
+  SET_STEP_SIZE,
+  SET_FREQ,
+  SELECT_VFO,
+  VFO_ON_OFF,
+  OUTPUT_DRIVE_LEVEL,
+  SET_CAL_FACTOR,
+  SAVE_INIT_VALS
+};
+
+enum ControlsState
+{
+  NOTHING,
+  ENC_SW_PRESSED,
+  ENC_UP,
+  ENC_DOWN
+};
+
+MenuStateValues menuState = SET_FREQ;
+
+void printCalFactor(void)
+{
+  char buffer[14];
+  itoa(calFactor/100, buffer, 10);
+  printStringToOLED(buffer);
+}
+
+void setCalFactor(void)
+{
+  uint8_t controlVal;
+  printCalFactor();
+  while (1)
+  {
+    controlVal = waitForControlChange();
+    if (controlVal == ENC_SW_PRESSED)
+      return;
+   else if (controlVal == ENC_UP)
+    {
+      calFactor += stepSize;
+      si5351.set_correction(calFactor, SI5351_PLL_INPUT_XO);
+      printCalFactor();
+    }
+    else if (controlVal == ENC_DOWN)
+    {
+      calFactor -= stepSize;
+      si5351.set_correction(calFactor, SI5351_PLL_INPUT_XO);
+      printCalFactor();
+    }
+  }
+}
+
+void saveInitValuesToEEPROM(void)
+{
+  #ifdef HAS_INTERNAL_EEPROM
+    storeEEPROM();
+    printStringToOLED("Stored");
+    delay(1000);
+  #endif
+}
+
+//
+void printVFOFreq(void)
+{
+  if (currentVFONumber == 0)
+    displayFreqInKHzOnOLED(float(VFO_0_Freq / 100ULL));
+  else if (currentVFONumber == 1)
+    displayFreqInKHzOnOLED(float(VFO_1_Freq / 100ULL));
+  else if (currentVFONumber == 2)
+    displayFreqInKHzOnOLED(float(VFO_2_Freq / 100ULL));
+}
+
+//
+void setVFOFreq(void)
+{
+  uint8_t controlVal;
+  printVFOFreq();
+  while (1)
+  {
+    controlVal = waitForControlChange();
+    if (controlVal == ENC_SW_PRESSED)
+      return;
+    else if (controlVal == ENC_UP)
+    {
+      if (currentVFONumber == 0)
+      {
+        VFO_0_Freq += stepSize; // count up by step size
+        if (VFO_0_Freq > 4000000000ULL)
+          VFO_0_Freq = 4000000000ULL; // Limit top of range
+        si5351.set_freq(VFO_0_Freq, SI5351_CLK0);
+      }
+      else if (currentVFONumber == 1)
+      {
+        VFO_1_Freq += stepSize; // count up by step size
+        if (VFO_1_Freq > 4000000000ULL)
+          VFO_1_Freq = 4000000000ULL; // Limit top of range
+        si5351.set_freq(VFO_1_Freq, SI5351_CLK1);
+      }
+      else if (currentVFONumber == 2)
+      {
+        VFO_2_Freq += stepSize; // count up by step size
+        if (VFO_2_Freq > 4000000000ULL)
+          VFO_2_Freq = 4000000000ULL; // Limit top of range
+        si5351.set_freq(VFO_2_Freq, SI5351_CLK2);
+      }
+    }
+    else if (controlVal == ENC_DOWN)
+    {
+      if (currentVFONumber == 0)
+      {
+        VFO_0_Freq -= stepSize; // count down by step size
+        if ((VFO_0_Freq < 1000000ULL) || (VFO_0_Freq > 4000000000ULL))
+          VFO_0_Freq = 1000000ULL;
+        si5351.set_freq(VFO_0_Freq, SI5351_CLK0);
+      }
+      else if (currentVFONumber == 1)
+      {
+        VFO_1_Freq -= stepSize; // count down by step size
+        if ((VFO_1_Freq < 1000000ULL) || (VFO_1_Freq > 4000000000ULL))
+          VFO_1_Freq = 1000000ULL;
+        si5351.set_freq(VFO_1_Freq, SI5351_CLK1);
+      }
+      else if (currentVFONumber == 2)
+      {
+        VFO_2_Freq -= stepSize; // count down by step size
+        if ((VFO_2_Freq < 1000000ULL) || (VFO_2_Freq > 4000000000ULL))
+          VFO_2_Freq = 1000000ULL;
+        si5351.set_freq(VFO_2_Freq, SI5351_CLK2);
+      }
+    }
+    printVFOFreq();
+  }
+}
+
+//
+void printVFONumber(void)
+{
+  if (currentVFONumber == 0)
+    printStringToOLED("VFO Clk0");
+  else if (currentVFONumber == 1)
+    printStringToOLED("VFO Clk1");
+  else if (currentVFONumber == 2)
+    printStringToOLED("VFO Clk2");
+}
+
+//enum ControlsState
+//{
+//  NOTHING,
+//  ENC_SW_PRESSED,
+//  ENC_UP,
+//  ENC_DOWN
+//};
+
+// enum si5351_drive {SI5351_DRIVE_2MA, SI5351_DRIVE_4MA, SI5351_DRIVE_6MA, SI5351_DRIVE_8MA};
+//uint8_t VFO_0_Drive = SI5351_DRIVE_4MA;
+//uint8_t VFO_1_Drive = SI5351_DRIVE_4MA;
+//uint8_t VFO_2_Drive = SI5351_DRIVE_4MA;
+
+si5351_drive printDriveStrength(void)
+{
+  int driveLevel;
+  if (currentVFONumber == 0)
+    driveLevel = VFO_0_Drive;
+  else if (currentVFONumber == 1)
+    driveLevel = VFO_1_Drive;
+  else if (currentVFONumber == 2)
+    driveLevel = VFO_2_Drive;
+  if (driveLevel == SI5351_DRIVE_2MA)
+  {
+    printStringToOLED("2mA drive");
+    return (SI5351_DRIVE_2MA);
+  }
+  else if (driveLevel == SI5351_DRIVE_4MA)
+  {
+    printStringToOLED("4mA drive");
+    return (SI5351_DRIVE_4MA);
+  }
+  else if (driveLevel == SI5351_DRIVE_6MA)
+  {
+    printStringToOLED("6mA drive");
+    return (SI5351_DRIVE_6MA);
+  }
+  else if (driveLevel == SI5351_DRIVE_8MA)
+  {
+    printStringToOLED("8mA drive");
+    return (SI5351_DRIVE_8MA);
+  }
+}
+
+void setDriveLevel(void)
+{
+  uint8_t controlVal;
+  si5351_drive driveVal;
+  while (1)
+  {
+    driveVal = printDriveStrength();
+    controlVal = waitForControlChange();
+    if (controlVal == ENC_SW_PRESSED)
+    {
+      if (currentVFONumber == 0)
+        si5351.drive_strength(SI5351_CLK0, driveVal);
+      else if (currentVFONumber == 1)
+        si5351.drive_strength(SI5351_CLK1, driveVal);
+      else if (currentVFONumber == 2)
+        si5351.drive_strength(SI5351_CLK2, driveVal);
+      return;
+    }
+    else if (controlVal == ENC_UP)
+    {
+      if (driveVal == SI5351_DRIVE_2MA)
+      {
+        driveVal = SI5351_DRIVE_4MA;
+      }
+      else if (driveVal == SI5351_DRIVE_4MA)
+      {
+        driveVal = SI5351_DRIVE_6MA;
+      }
+      else if (driveVal == SI5351_DRIVE_6MA)
+      {
+        driveVal = SI5351_DRIVE_8MA;
+      }
+    }
+    else if (controlVal == ENC_DOWN)
+    {
+      if (driveVal == SI5351_DRIVE_8MA)
+      {
+        driveVal = SI5351_DRIVE_6MA;
+      }
+      else if (driveVal == SI5351_DRIVE_6MA)
+      {
+        driveVal = SI5351_DRIVE_4MA;
+      }
+      else if (driveVal == SI5351_DRIVE_4MA)
+      {
+        driveVal = SI5351_DRIVE_2MA;
+      }
+    }
+    if (currentVFONumber == 0)
+      VFO_0_Drive = driveVal;
+    else if (currentVFONumber == 1)
+      VFO_1_Drive = driveVal;
+    else if (currentVFONumber == 2)
+      VFO_2_Drive = driveVal;
+  }
+}
+
+void selectVFO(void)
+{
+  uint8_t controlVal;
+  printVFONumber();
+  while (1)
+  {
+    controlVal = waitForControlChange();
+    if (controlVal == ENC_SW_PRESSED)
+      return;
+    else if (controlVal == ENC_UP)
+    {
+      if (currentVFONumber == 0)
+        currentVFONumber = 1;
+      else if (currentVFONumber == 1)
+        currentVFONumber = 2;
+    }
+    else if (controlVal == ENC_DOWN)
+    {
+      if (currentVFONumber == 2)
+        currentVFONumber = 1;
+      else if (currentVFONumber == 1)
+        currentVFONumber = 0;
+    }
+    printVFONumber();
+  }
+}
+
+void printVFOOnOff(void)
+{
+  if (currentVFONumber == 0)
+  {
+    if (VFO_O_OnOff == VFO_ON)
+      printStringToOLED("VFO Clk0 On");
+    else
+      printStringToOLED("VFO Clk0 Off");
+  }
+  else if (currentVFONumber == 1)
+  {
+    if (VFO_1_OnOff == VFO_ON)
+      printStringToOLED("VFO Clk1 On");
+    else
+      printStringToOLED("VFO Clk1 Off");
+  }
+  else if (currentVFONumber == 2)
+  {
+    if (VFO_2_OnOff == VFO_ON)
+      printStringToOLED("VFO Clk2 On");
+    else
+      printStringToOLED("VFO Clk2 Off");
+  }
+}
+
+void toggleVFOOnOff(void)
+{
+  uint8_t controlVal;
+  printVFOOnOff();
+  while (1)
+  {
+    controlVal = waitForControlChange();
+    if (controlVal == ENC_SW_PRESSED)
+      return;
+    else if (controlVal == ENC_UP)
+    {
+      if (currentVFONumber == 0)
+      {
+        VFO_O_OnOff = VFO_ON;
+        si5351.output_enable(SI5351_CLK0, 1);
+      }
+      else if (currentVFONumber == 1)
+      {
+        VFO_1_OnOff = VFO_ON;
+        si5351.output_enable(SI5351_CLK1, 1);
+      }
+      else if (currentVFONumber == 2)
+        VFO_2_OnOff = VFO_ON;
+      si5351.output_enable(SI5351_CLK2, 1);
+    }
+    else if (controlVal == ENC_DOWN)
+    {
+      if (currentVFONumber == 0)
+      {
+        VFO_O_OnOff = VFO_OFF;
+        si5351.output_enable(SI5351_CLK0, 0);
+      }
+      else if (currentVFONumber == 1)
+      {
+        VFO_1_OnOff = VFO_OFF;
+        si5351.output_enable(SI5351_CLK1, 0);
+      }
+      else if (currentVFONumber == 2)
+      {
+        VFO_2_OnOff = VFO_OFF;
+        si5351.output_enable(SI5351_CLK2, 0);
+      }
+    }
+    printVFOOnOff();
+  }
+}
+
+void printStepSize(void)
+{
+  if (stepSize == STEP_1_HZ)
+    printStringToOLED("Step 1 Hz");
+  else if (stepSize == STEP_10_HZ)
+    printStringToOLED("Step 10 Hz");
+  else if (stepSize == STEP_100_HZ)
+    printStringToOLED("Step 100 Hz");
+  else if (stepSize == STEP_1_KHZ)
+    printStringToOLED("Step 1 KHz");
+  else if (stepSize == STEP_10_KHZ)
+    printStringToOLED("Step 10 KHz");
+  else if (stepSize == STEP_100_KHZ)
+    printStringToOLED("Step 100 KHz");
+  else if (stepSize == STEP_1_MHZ)
+    printStringToOLED("Step 1 MHz");
+  else if (stepSize == STEP_10_MHZ)
+    printStringToOLED("Step 10 MHz");
+  return;
+}
+
+void setVFOStepSize(void)
+{
+  uint8_t controlVal;
+  printStepSize();
+  while (1)
+  {
+    controlVal = waitForControlChange();
+    if (controlVal == ENC_SW_PRESSED)
+      return;
+    else if (controlVal == ENC_UP)
+    {
+      if (stepSize == STEP_1_HZ)
+        stepSize = STEP_10_HZ;
+      else if (stepSize == STEP_10_HZ)
+        stepSize = STEP_100_HZ;
+      else if (stepSize == STEP_100_HZ)
+        stepSize = STEP_1_KHZ;
+      else if (stepSize == STEP_1_KHZ)
+        stepSize = STEP_10_KHZ;
+      else if (stepSize == STEP_10_KHZ)
+        stepSize = STEP_100_KHZ;
+      else if (stepSize == STEP_100_KHZ)
+        stepSize = STEP_1_MHZ;
+      else if (stepSize == STEP_1_MHZ)
+        stepSize = STEP_10_MHZ;
+    }
+    else if (controlVal == ENC_DOWN)
+    {
+      if (stepSize == STEP_10_HZ)
+        stepSize = STEP_1_HZ;
+      else if (stepSize == STEP_100_HZ)
+        stepSize = STEP_10_HZ;
+      else if (stepSize == STEP_1_KHZ)
+        stepSize = STEP_100_HZ;
+      else if (stepSize == STEP_10_KHZ)
+        stepSize = STEP_1_KHZ;
+      else if (stepSize == STEP_100_KHZ)
+        stepSize = STEP_10_KHZ;
+      else if (stepSize == STEP_1_MHZ)
+        stepSize = STEP_100_KHZ;
+      else if (stepSize == STEP_10_MHZ)
+        stepSize = STEP_1_MHZ;
+    }
+    printStepSize();
+  }
+
+}
+
+void displayTopMenuOption()
+{
+  if (menuState == SET_STEP_SIZE)
+    printStringToOLED("Set Step Size");
+  else if (menuState == SET_FREQ)
+    printStringToOLED("Set Freq");
+  else if (menuState == SELECT_VFO)
+    printStringToOLED("Select VFO");
+  else if (menuState == VFO_ON_OFF)
+    printStringToOLED("VFO On/Off");
+  else if (menuState == OUTPUT_DRIVE_LEVEL)
+    printStringToOLED("Drive Level");
+  else if (menuState == SET_CAL_FACTOR)
+    printStringToOLED("Set Cal Value");
+  else if (menuState == SAVE_INIT_VALS)
+    printStringToOLED("Save defaults");
+}
+
+//enum MenuStateValues
+//{
+//  SET_STEP_SIZE,
+//  SET_FREQ,
+//  SELECT_VFO,
+//  VFO_ON_OFF,
+//  SET_CAL_FACTOR,
+//  SAVE_INIT_VALS
+//};
+
+// Menu options
+// Menu is entered by pressing encoder button
+void vfoMenu(void)
+{
+  uint8_t controlVal;
+  while (1)
+  {
+    displayTopMenuOption();
+    controlVal = waitForControlChange();
+    if (controlVal == ENC_SW_PRESSED)
+    {
+      if (menuState == SET_STEP_SIZE)
+        setVFOStepSize();
+      else if (menuState == SET_FREQ)
+        setVFOFreq();
+      else if (menuState == SELECT_VFO)
+        selectVFO();
+      else if (menuState == VFO_ON_OFF)
+        toggleVFOOnOff();
+      else if (menuState == OUTPUT_DRIVE_LEVEL)
+        setDriveLevel();
+      else if (menuState == SET_CAL_FACTOR)
+        setCalFactor();
+      else if (menuState == SAVE_INIT_VALS)
+        saveInitValuesToEEPROM();
+    }
+    else if (controlVal == ENC_UP)
+    {
+      if (menuState == SET_STEP_SIZE)
+        menuState = SET_FREQ;
+      else if (menuState == SET_FREQ)
+        menuState = SELECT_VFO;
+      else if (menuState == SELECT_VFO)
+        menuState = VFO_ON_OFF;
+      else if (menuState == VFO_ON_OFF)
+        menuState = OUTPUT_DRIVE_LEVEL;
+      else if (menuState == OUTPUT_DRIVE_LEVEL)
+        menuState = SET_CAL_FACTOR;
+      else if (menuState == SET_CAL_FACTOR)
+        menuState = SAVE_INIT_VALS;
+    }
+    else if (controlVal == ENC_DOWN)
+    {
+      if (menuState == SAVE_INIT_VALS)
+        menuState = SET_CAL_FACTOR;
+      else if (menuState == SET_CAL_FACTOR)
+        menuState = OUTPUT_DRIVE_LEVEL;
+      else if (menuState == OUTPUT_DRIVE_LEVEL)
+        menuState = VFO_ON_OFF;
+      else if (menuState == VFO_ON_OFF)
+        menuState = SELECT_VFO;
+      else if (menuState == SELECT_VFO)
+        menuState = SET_FREQ;
+      else if (menuState == SET_FREQ)
+        menuState = SET_STEP_SIZE;
+    }
+  }
+}
+
+uint8_t checkControls()
+{
+  int encoderDelta = 0;
+  if (checkSwitch() == 1)
+    return ENC_SW_PRESSED;
+  pollEncoder();
+  encoderDelta = getEncoderDelta();
+  if (encoderDelta == 0)
+    return (NOTHING);
+  else if (encoderDelta == 1)
+    return (ENC_UP);
+  else if (encoderDelta == -1)
+    return (ENC_DOWN);
+  return (NOTHING);
+}
+
+// Wait around for an encoder change
+uint8_t waitForControlChange(void)
+{
+  uint8_t controlVal;
+  while (1)
+  {
+    do
+    {
+      controlVal = checkControls();
+    }
+    while (controlVal == NOTHING);
+    return (controlVal);
+  }
+}
