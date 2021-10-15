1183b31c17d251a29032e72681ab52989cca9087
Doug Gilliland <doug@douglasgilliland.com>
2021-10-15 11:29:15 -0400

    Update U8X8Test.ino

--- LBCards/U8X8Test/U8X8Test.ino
+++ LBCards/U8X8Test/U8X8Test.ino
@@ -1,4 +1,5 @@
 // https://github.com/olikraus/u8g2/wiki/u8x8reference#c-example
+// https://github.com/olikraus/u8g2/wiki/fntlist8x8
 
 #include <Arduino.h>
 #include <SPI.h>
@@ -9,14 +10,11 @@
 U8X8_SSD1306_128X32_UNIVISION_HW_I2C u8x8(U8X8_PIN_NONE);
 
 void setup() {
-  // put your setup code here, to run once:
   u8x8.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   u8x8.setFont(u8x8_font_victoriabold8_r);
   u8x8.drawString(0,0,"Hello World!");
   delay(1000);
-
 }
