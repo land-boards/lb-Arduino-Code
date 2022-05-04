f0e9a972d8835d44cd07d71f6fb73c08590541e5
Doug Gilliland <doug@douglasgilliland.com>
2022-05-04 10:17:32 -0400

    Update TinyPS2V03.ino

--- LBCards/TinyPS2/TinyPS2V03/TinyPS2V03.ino
+++ LBCards/TinyPS2/TinyPS2V03/TinyPS2V03.ino
@@ -2,17 +2,12 @@
 // Write Serial data
 
 #include <SoftwareSerial.h>
-#include <PS2Keyboard.h>
-
-const int DataPin = 0;
-const int IRQpin =  1;
 
 #define rxPin 3
 #define txPin 2
 
 // Set up SoftwareSerial and PS/2 objects
 SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin);
-PS2Keyboard keyboard;
 
 void setup() {
 
@@ -20,7 +15,6 @@ void setup() {
   pinMode(txPin, OUTPUT);
   mySerial.begin(9600);
   
-  keyboard.begin(DataPin, IRQpin);
 }
 
 void loop() 
@@ -31,11 +25,4 @@ void loop()
     for (c = 'A'; c <= 'Z'; c++)
       mySerial.write(c);
   }
-//  if (keyboard.available()) 
-//  {
-//    
-//    // read the next key
-//    char c = keyboard.read();
-//    mySerial.write(c);
-//  }
 }
