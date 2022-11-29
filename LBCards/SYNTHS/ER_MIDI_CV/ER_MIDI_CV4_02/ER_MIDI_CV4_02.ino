dce915bc8890c590c0ef062321444f2197879088
Doug Gilliland <doug@douglasgilliland.com>
2022-11-29 05:22:36 -0500

    Added many comments

--- LBCards/SYNTHS/ER_MIDI_CV/ER_MIDI_CV4_02/ER_MIDI_CV4_02.ino
+++ LBCards/SYNTHS/ER_MIDI_CV/ER_MIDI_CV4_02/ER_MIDI_CV4_02.ino
@@ -1,22 +1,39 @@
 //  ER_MIDI_CV4_02
+//    MIDI controller, 4 CV outputs
 //
 //  Card:
 //    http://land-boards.com/blwiki/index.php?title=ER-MIDI-CV4-02
+//    Arduino Pro Mini CPU, 5V, 16 MHz
+//    (2) MCP4822 Dual DACs
+//    Optisolated MIDI input on TRS connector
 //
 //  BASED ON HAGIWO MIDI to CV code
 //    https://note.com/solder_state/n/n17e028497eba
 //    Japanese to English via google translate
+//
+// Download steps
+//  Unplug MIDI in cable from front panel
+//  Connect FTDI card with 1:1 cable
+//  Download code in Arduino IDE
+//  Disconnect FTDI
+//  Connect MIDI in cable to front panel
+//
 
 #include <MIDI.h>
-#include  <SPI.h>//DAC for communication
-MIDI_CREATE_DEFAULT_INSTANCE(); // USB-MIDI Enable Library
+#include <SPI.h>    // Communicate with DAC using SPI
+MIDI_CREATE_DEFAULT_INSTANCE(); // Enable USB-MIDI Library
 
+// Arduino pin defines
 #define LDAC1_PIN 9  // DAC TRANSFER STROBE 1
 #define LDAC2_PIN 8  // DAC TRANSFER STROBE 2
 #define SS_PIN 10    // SLAVE SELECT PIN
 #define CLK_PIN 4    // CLOCK PIN
 #define GATE_PIN 5   // GATE PIN
 #define TRIG_PIN 6   // TRIGGER PIN
+
+// https://www.midi.org/specifications-old/item/table-3-control-change-messages-data-bytes-2
+#define MIDI_CTRL_ALL_SOUND_OFF 120
+#define MIDI_CTRL_ALL_NOTES_OFF 123
 
 int note_no = 0;  //noteNo=21(A0)～60(A5) total 61, Because it takes a negative value int
 
@@ -25,13 +42,17 @@ int bend_msb = 0;
 int bend_lsb = 0;
 long after_bend_pitch = 0;
 
+// Keep the CV values since they are all re-written when any change
 uint16_t CV1Val = 0;
 uint16_t CV2Val = 0;
 uint16_t CV3Val = 0;
 uint16_t CV4Val = 0;
 
-byte note_on_count = 0;//When multiple notes are ON and one of the notes is turned off, the last note ON does not disappear.
-unsigned long trigTimer = 0;//for gate ratch
+// When multiple notes are ON and one of the notes is turned off,
+//   the last note ON does not disappear.
+// This needs improvement
+byte note_on_count = 0;
+unsigned long trigTimer = 0;  // Timer for gate output
 
 byte clock_count = 0;
 byte clock_max = 24;//clock_max change by knob setting
@@ -39,70 +60,130 @@ byte clock_on_time = 0;
 int clock_rate = 0;//knob CVin
 
 // V/OCT LSB for DAC
-//const long cv[61] = {
-//  0,  68, 137,  205,  273,  341,  410,  478,  546,  614,  683,  751,
-//  819,  887,  956,  1024, 1092, 1161, 1229, 1297, 1365, 1434, 1502, 1570,
-//  1638, 1707, 1775, 1843, 1911, 1980, 2048, 2116, 2185, 2253, 2321, 2389,
-//  2458, 2526, 2594, 2662, 2731, 2799, 2867, 2935, 3004, 3072, 3140, 3209,
-//  3277, 3345, 3413, 3482, 3550, 3618, 3686, 3755, 3823, 3891, 3959, 4028, 4095
-//};
-// 
+// ER-VCO-03 characteristics
+//  PITCH ADJ knob roughly centered
+//  0V = 93.08 Hz (roughly F2)
+//  1V = 179 Hz (G3)
+//  1.52V = 261 Hz (C4)
+//  2V = 365 Hz (G4)
+//  3V = 722 Hz (G5)
+//  4V = 1.45 KHz (G6)
+//  5V = 3.05 KHz (G7)
+// Max output voltage of this card is 4.096V (4 octaves)
+//  1mV per DAC count
+// 12 notes per octave = 1/12V per note = 83.333 mV
+// Steps are in 83.333 mV
+// Table counts up by integer steps - rounded 83.333 mV
+// Generated table in spreadsheet
 const long cv[60] = {
-  0,    83,   152,  220,  288,  356,  425,  493,  561,  629,  698,  766,
-  834,  903,  971,  1039, 1107, 1176, 1244, 1312, 1380, 1449, 1517, 1585,
-  1653, 1722, 1790, 1858, 1927, 1995, 2063, 2131, 2200, 2268, 2336, 2404,
-  2473, 2541, 2609, 2677, 2746, 2814, 2882, 2951, 3019, 3087, 3155, 3224,
-  3292, 3360, 3428, 3497, 3565, 3633, 3701, 3770, 3838, 3906, 3975, 4043
+  0,
+  83,
+  167,
+  250,
+  333,
+  417,
+  500,
+  583,
+  667,
+  750,
+  833,
+  917,
+  1000,
+  1083,
+  1167,
+  1250,
+  1333,
+  1417,
+  1500,
+  1583,
+  1667,
+  1750,
+  1833,
+  1917,
+  2000,
+  2083,
+  2167,
+  2250,
+  2333,
+  2417,
+  2500,
+  2583,
+  2667,
+  2750,
+  2833,
+  2917,
+  3000,
+  3083,
+  3167,
+  3250,
+  3333,
+  3417,
+  3500,
+  3583,
+  3667,
+  3750,
+  3833,
+  3917,
+  4000,
+  4083
 };
 
 // setup()
 
 void setup() {
-  pinMode(LDAC1_PIN, OUTPUT);//DAC trans
-  pinMode(LDAC2_PIN, OUTPUT);//DAC trans
-  pinMode(SS_PIN, OUTPUT);//DAC trans
+  pinMode(LDAC1_PIN, OUTPUT);   //DAC1 transfer strobe
+  pinMode(LDAC2_PIN, OUTPUT);   //DAC2 transfer strobe
+  pinMode(SS_PIN, OUTPUT);      //DAC Slave Select
   digitalWrite(LDAC1_PIN, HIGH);
   digitalWrite(LDAC2_PIN, HIGH);
 
-  pinMode(CLK_PIN, OUTPUT);//CLK_OUT
-  pinMode(GATE_PIN, OUTPUT);//GATE_OUT
+  pinMode(CLK_PIN, OUTPUT);     // CLK_OUT
+  pinMode(TRIG_PIN, OUTPUT);    // TRIG_OUT
+  pinMode(GATE_PIN, OUTPUT);    //GATE_OUT
   digitalWrite(CLK_PIN, LOW);
   digitalWrite(TRIG_PIN, LOW);
   digitalWrite(GATE_PIN, LOW);
 
-  MIDI.begin(1);          // MIDI CH1 to listen
+  MIDI.begin(1);          // Listen to MIDI Channel 1
 
-  SPI.begin();
+  SPI.begin();                        // Set up SPI for DAC transfers
   SPI.setBitOrder(MSBFIRST);          // bit order
   SPI.setClockDivider(SPI_CLOCK_DIV4);// Clock (CLK) is used for 1/4 of the system clock (16MHz/4)
   SPI.setDataMode(SPI_MODE0);         // Clock polarity 0 (LOW)　Clock Phase 0
+  // Set the CV values
   CV1Val = 0;
   CV2Val = 0;
   CV3Val = 0;
   CV4Val = 0;
+  // Write out the initial CV values
   outCVs();
-  delay(50);
+  delay(50);  // For grins
 }
 
 void loop() {
 
   //-----------------------------clock_rate setting----------------------------
+  // CLK ADJ knob has 4 clock divisor ranges
   clock_rate = analogRead(1);//read knob voltage
 
-  if (clock_rate < 256) {
+  if (clock_rate < 256)
+  {
     clock_max = 24;//slow
   }
-  else if (clock_rate < 512 && clock_rate >= 256) {
+  else if (clock_rate < 512 && clock_rate >= 256)
+  {
     clock_max = 12;
   }
-  else if (clock_rate < 768 && clock_rate >= 512) {
+  else if (clock_rate < 768 && clock_rate >= 512)
+  {
     clock_max = 6;
   }
-  else if (clock_rate >= 768) {
+  else if (clock_rate >= 768)
+  {
     clock_max = 3;//fast
   }
 
-  //-----------------------------gate ratch----------------------------
+  //----------------------------- GATE signal ----------------------------
   if (note_on_count != 0) {
     if ((millis() - trigTimer <= 20) && (millis() - trigTimer > 10)) {
       digitalWrite(GATE_PIN, LOW);
@@ -117,17 +198,21 @@ void loop() {
     MIDI.setInputChannel(1);
     switch (MIDI.getType()) {
 
+      // https://www.inspiredacoustics.com/en/MIDI_note_numbers_and_center_frequencies
       case midi::NoteOn://NoteOn After
         note_on_count ++;
         trigTimer = millis();
-//        note_no = MIDI.getData1() - 21; //note number
-        note_no = MIDI.getData1() - 43;   //note number lowest on ER-VCO-03
+        //        note_no = MIDI.getData1() - 21; //note number
+        // lowest note number on ER-VCO-03 w/ pot centered
+        note_no = MIDI.getData1() - 43;   // G2 is MIDI note 43
 
-        if (note_no < 0) {
+        if (note_no < 0)
+        {
           note_no = 0;
         }
-        else if (note_no >= 60) {
-          note_no = 60;
+        else if (note_no >= 50) // Just a bit more than 4 octaves A#6/Bb6 = Highest note
+        {
+          note_no = 50;
         }
 
         CV1Val = cv[note_no];
@@ -137,14 +222,23 @@ void loop() {
 
       case midi::NoteOff://NoteOff After
         note_on_count --;
-        if (note_on_count == 0) {
+        if (note_on_count == 0)
+        {
           digitalWrite(GATE_PIN, LOW); //Gate to LOW
         }
         break;
 
       case midi::ControlChange:
-        CV2Val = MIDI.getData2() << 5;
-        outCVs();
+        uint8_t changeVal = MIDI.getData2();
+        if ((changeVal == MIDI_CTRL_ALL_NOTES_OFF) || (changeVal == MIDI_CTRL_ALL_SOUND_OFF))
+        {
+          digitalWrite(GATE_PIN, LOW); //Gate to LOW
+          CV1Val = 0;
+          CV2Val = 0;
+          CV3Val = 0;
+          CV4Val = 0;
+          outCVs();
+        }
         break;
 
       case midi::Clock:
