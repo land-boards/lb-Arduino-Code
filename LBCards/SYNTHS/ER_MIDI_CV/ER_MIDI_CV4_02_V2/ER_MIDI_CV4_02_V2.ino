2d568ecb0fa68942455fb69b8d17f4c56dfefa26
Doug Gilliland <doug@douglasgilliland.com>
2023-01-09 22:25:44 -0500

    Update ER_MIDI_CV4_02_V2.ino

--- LBCards/SYNTHS/ER_MIDI_CV/ER_MIDI_CV4_02_V2/ER_MIDI_CV4_02_V2.ino
+++ LBCards/SYNTHS/ER_MIDI_CV/ER_MIDI_CV4_02_V2/ER_MIDI_CV4_02_V2.ino
@@ -4,6 +4,7 @@
 //  Card:
 //    http://land-boards.com/blwiki/index.php?title=ER-MIDI-CV4-02
 //    Arduino Pro Mini CPU, 5V, 16 MHz
+//      ATMega168 or ATMega328
 //    (2) MCP4822 Dual DACs
 //      0-4.096V swing is ~4 octaves
 //    Optisolated MIDI input on TRS connector
@@ -11,8 +12,11 @@
 //  BASED ON HAGIWO MIDI to CV code
 //    https://note.com/solder_state/n/n17e028497eba
 //    Japanese to English via google translate
-//    Changed to play single note only
+//    Changed to be able to play "polyphonic"
+//      See POLYPHONIC define below to use polyphonic mode
 //      First note to arrive plays until note is done
+//      Single GATE out - This limitation is due to a single ADSR in my sythn
+//    Uses TRIG and CLK as outs for drums in Beaks (Peaks)
 //
 // Download steps
 //  Unplug MIDI in cable from front panel
@@ -24,14 +28,26 @@
 //  Connect MIDI in cable to front panel
 //
 // ATMega168 Resources
-// Sketch uses 5444 bytes (37%) of program storage space. Maximum is 14336 bytes.
-// Global variables use 512 bytes (50%) of dynamic memory, leaving 512 bytes for local variables.
+//  Sketch uses 5304 bytes (36%) of program storage space. Maximum is 14336 bytes.
+//  Global variables use 512 bytes (50%) of dynamic memory, leaving 512 bytes for local variables.
 //    Maximum is 1024 bytes.
 
 
 #include <MIDI.h>
 #include <SPI.h>    // Communicate with DAC using SPI
 MIDI_CREATE_DEFAULT_INSTANCE(); // Enable MIDI Library
+
+///////////////////////////////////////////////////////////////////////
+// Configurations
+
+// Can also write in pairs (1&2 oe 3&4) instead of all 4
+#define POLYPHONIC   // if polyphonic
+//#undef POLYPHONIC      // if not polyphonic
+
+// Use the CLk and TRIG outputs to Beaks (Peaks) control drums
+#define DRUM_TRIGGER_OUTS
+//#undef DRUM_TRIGGER_OUTS
+///////////////////////////////////////////////////////////////////////
 
 // Arduino pin defines
 #define LDAC1_PIN 9  // DAC TRANSFER STROBE 1
@@ -41,6 +57,27 @@ MIDI_CREATE_DEFAULT_INSTANCE(); // Enable MIDI Library
 #define GATE_PIN 5   // GATE PIN
 #define TRIG_PIN 6   // TRIGGER PIN
 
+// Uses direct port manipulation to set/clear outputs
+//  https://docs.arduino.cc/hacking/software/PortManipulation
+//    B (digital pin 8 to 13)
+//    C (analog input pins)
+//    D (digital pins 0 to 7)
+// Macros for the digitalWrite functions
+#define SET_CLK_HIGH PORTD = PORTD | 0X10
+#define SET_CLK_LOW PORTD = PORTD & 0XEF
+#define SET_GATE_HIGH PORTD = PORTD | 0X20
+#define SET_GATE_LOW PORTD = PORTD & 0XDF
+#define SET_TRIG_HIGH PORTD = PORTD | 0X40
+#define SET_TRIG_LOW PORTD = PORTD & 0XBF
+#define SET_ALL_CTRLS_LOW PORTD = PORTD & 0X8F
+#define SET_LDAC1_HIGH PORTB = PORTB | 0X02
+#define SET_LDAC1_LOW PORTB = PORTB & 0XFD
+#define SET_LDAC2_HIGH PORTB = PORTB | 0X01
+#define SET_LDAC2_LOW PORTB = PORTB & 0XFE
+#define SET_SS_HIGH PORTB = PORTB | 0X04
+#define SET_SS_LOW PORTB = PORTB & 0XFB
+
+// Control change messages
 // https://www.midi.org/specifications-old/item/table-3-control-change-messages-data-bytes-2
 #define MIDI_CTRL_ALL_SOUND_OFF 120
 #define MIDI_CTRL_ALL_NOTES_OFF 123
@@ -60,11 +97,12 @@ uint16_t CV4Val = 0;
 
 // When multiple notes are ON and one of the notes is turned off,
 //   the last note ON does not disappear.
-//byte note_on_count = 0;
 unsigned long gateTimer1 = 0;  // Timer for gate output
+#ifdef POLYPHONIC
 unsigned long gateTimer2 = 0;  // Timer for gate output
 unsigned long gateTimer3 = 0;  // Timer for gate output
 unsigned long gateTimer4 = 0;  // Timer for gate output
+#endif
 
 byte clock_count = 0;
 byte clock_max = 24;//clock_max change by knob setting
@@ -73,14 +111,18 @@ int clock_rate = 0;//knob CVin
 
 // Flags/values for note tracking
 bool noteOnFlag1;    // True if note is being played
+#ifdef POLYPHONIC
 bool noteOnFlag2;    // True if note is being played
 bool noteOnFlag3;    // True if note is being played
 bool noteOnFlag4;    // True if note is being played
+#endif
 int8_t noteOnVal;  // MIDI note being played value
 int8_t pianoNoteOnVal1;  // MIDI note being played value
+#ifdef POLYPHONIC
 int8_t pianoNoteOnVal2;  // MIDI note being played value
 int8_t pianoNoteOnVal3;  // MIDI note being played value
 int8_t pianoNoteOnVal4;  // MIDI note being played value
+#endif
 int8_t noteOffVal; // Used to check the note being turned off is the right one
 
 // V/OCT LSB for DAC
@@ -114,15 +156,13 @@ void setup()
   pinMode(LDAC1_PIN, OUTPUT);   //DAC1 transfer strobe
   pinMode(LDAC2_PIN, OUTPUT);   //DAC2 transfer strobe
   pinMode(SS_PIN, OUTPUT);      //DAC Slave Select
-  digitalWrite(LDAC1_PIN, HIGH);
-  digitalWrite(LDAC2_PIN, HIGH);
+  SET_LDAC1_HIGH;
+  SET_LDAC2_HIGH;
 
   pinMode(CLK_PIN, OUTPUT);     // CLK_OUT
   pinMode(TRIG_PIN, OUTPUT);    // TRIG_OUT
-  pinMode(GATE_PIN, OUTPUT);    //GATE_OUT
-  digitalWrite(CLK_PIN, LOW);
-  digitalWrite(TRIG_PIN, LOW);
-  digitalWrite(GATE_PIN, LOW);
+  pinMode(GATE_PIN, OUTPUT);    // GATE_OUT
+  SET_ALL_CTRLS_LOW;
 
   MIDI.begin(1);          // Listen to MIDI Channel 1
 
@@ -138,12 +178,14 @@ void setup()
   // Write out the initial CV values
   outCVs();
   noteOnFlag1 = false;
+#ifdef POLYPHONIC
   noteOnFlag2 = false;
   noteOnFlag3 = false;
   noteOnFlag4 = false;
+#endif
   noteOnVal = 0;
   delay(50);  // For grins
-//  Serial.begin(9600);
+  //  Serial.begin(9600);
 }
 
 #define DAC_LOWEST_NOTE 43    // G2 is MIDI note 43
@@ -177,25 +219,27 @@ void loop()
         //note_on_count ++;
         noteOnVal = MIDI.getData1();
         // Drum triggers on GATE/CLK outputs
-        // Bottom 6 keys of keyboard
+        // Bottom 6 keys of 61 key keyboard
+#ifdef DRUM_TRIGGER_OUTS
         if (noteOnVal <= 39)
         {
-          digitalWrite(CLK_PIN, HIGH);
+          SET_CLK_HIGH;
           delay(1);
-          digitalWrite(CLK_PIN, LOW);
+          SET_CLK_LOW;
         }
         else if (noteOnVal < 43)
         {
-          digitalWrite(TRIG_PIN, HIGH);
+          SET_TRIG_HIGH;
           delay(1);
-          digitalWrite(TRIG_PIN, LOW);
+          SET_TRIG_LOW;
         }
         else
+#endif
         {
           if (noteOnFlag1 == false)
           {
             gateTimer1 = millis();
-            //        note_no = MIDI.getData1() - 21; //note number
+            // note_no = MIDI.getData1() - 21; //note number
             // lowest note number on ER-VCO-03 w/ pot centered
             note_no = noteOnVal - DAC_LOWEST_NOTE;   // G2 is MIDI note 43
             if (note_no < 0)
@@ -206,11 +250,12 @@ void loop()
             {
               CV1Val = cv[note_no];
               outCVs_1_2();
-              digitalWrite(GATE_PIN, HIGH); //Gate to HIGH
+              SET_GATE_HIGH;
               noteOnFlag1 = true;
               pianoNoteOnVal1 = noteOnVal;
             }
           }
+#ifdef POLYPHONIC
           else if (noteOnFlag2 == false)
           {
             gateTimer2 = millis();
@@ -225,7 +270,7 @@ void loop()
             {
               CV2Val = cv[note_no];
               outCVs_1_2();
-              digitalWrite(GATE_PIN, HIGH); //Gate to HIGH
+              SET_GATE_HIGH;
               noteOnFlag2 = true;
               pianoNoteOnVal2 = noteOnVal;
             }
@@ -244,7 +289,7 @@ void loop()
             {
               CV3Val = cv[note_no];
               outCVs_3_4();
-              digitalWrite(GATE_PIN, HIGH); //Gate to HIGH
+              SET_GATE_HIGH;
               noteOnFlag3 = true;
               pianoNoteOnVal3 = noteOnVal;
             }
@@ -263,11 +308,12 @@ void loop()
             {
               CV4Val = cv[note_no];
               outCVs_3_4();
-              digitalWrite(GATE_PIN, HIGH); //Gate to HIGH
+              SET_GATE_HIGH;
               noteOnFlag4 = true;
               pianoNoteOnVal4 = noteOnVal;
             }
           }
+#endif
         }
         break;
 
@@ -276,6 +322,7 @@ void loop()
         noteOffVal = MIDI.getData1();
         if ((noteOffVal == pianoNoteOnVal1) && noteOnFlag1)
           noteOnFlag1 = false;
+#ifdef POLYPHONIC
         else if ((noteOffVal == pianoNoteOnVal2) && noteOnFlag2)
           noteOnFlag2 = false;
         else if ((noteOffVal == pianoNoteOnVal3) && noteOnFlag3)
@@ -283,33 +330,49 @@ void loop()
         else if ((noteOffVal == pianoNoteOnVal4) && noteOnFlag4)
           noteOnFlag4 = false;
         if (!noteOnFlag1 & !noteOnFlag2 & !noteOnFlag3 & !noteOnFlag4)
-          digitalWrite(GATE_PIN, LOW); //Gate to LOW
+          SET_GATE_LOW;
+#endif
+#ifndef POLYPHONIC
+        if (!noteOnFlag1)
+          SET_GATE_LOW;
+#endif
         break;
 
       case midi::ControlChange:
         uint8_t changeVal = MIDI.getData2();
         if ((changeVal == MIDI_CTRL_ALL_NOTES_OFF) || (changeVal == MIDI_CTRL_ALL_SOUND_OFF))
         {
-          digitalWrite(GATE_PIN, LOW); //Gate to LOW
+          SET_ALL_CTRLS_LOW;
+          // Set all the flags in case there is a lingering note
           noteOnFlag1 = false;
+#ifdef POLYPHONIC
+          noteOnFlag2 = false;
+          noteOnFlag3 = false;
+          noteOnFlag4 = false;
+#endif
         }
         break;
 
       case midi::Clock:
+        // Clock is "overloaded" output when drums are on TRIG and CLk
+#ifndef DRUM_TRIGGER_OUTS
         clock_count ++;
 
         if (clock_count >= clock_max)
           clock_count = 0;
 
         if (clock_count == 1)
-          digitalWrite(CLK_PIN, HIGH);
+          SET_CLK_HIGH;
         else if (clock_count != 1)
-          digitalWrite(CLK_PIN, LOW);
+          SET_CLK_LOW;
+#endif
         break;
 
       case midi::Stop:
+#ifndef DRUM_TRIGGER_OUTS
         clock_count = 0;
-        digitalWrite(GATE_PIN, LOW); //Gate to LOW
+        SET_CLK_LOW;
+#endif
         break;
 
       case midi::PitchBend:
@@ -329,9 +392,6 @@ void loop()
           CV1Val = after_bend_pitch;
           outCVs_1_2();
         }
-//        Serial.print("bend_msb ");
-//        Serial.print(bend_msb);
-//        Serial.println("");
         break;
     }
   }
@@ -345,19 +405,19 @@ void outCVs_1_2()
   CV1Low =  CV1Val & 0xff;
   CV2High = CV2Val >> 8 | 0x90;
   CV2Low =  CV2Val & 0xff;
-  digitalWrite(SS_PIN, LOW);
-  SPI.transfer(CV1High);   // H0x30=OUTA/1x
+  SET_SS_LOW;
+  SPI.transfer(CV1High);   // 0x30=OUTA/1x
   SPI.transfer(CV1Low);
-  digitalWrite(SS_PIN, HIGH);
+  SET_SS_HIGH;
   delayMicroseconds(2);
-  digitalWrite(SS_PIN, LOW);
-  SPI.transfer(CV2High);   // H0x30=OUTA/1x
+  SET_SS_LOW;
+  SPI.transfer(CV2High);   // 0x30=OUTA/1x
   SPI.transfer(CV2Low);
-  digitalWrite(SS_PIN, HIGH);
+  SET_SS_HIGH;
   delayMicroseconds(2);
-  digitalWrite(LDAC1_PIN, LOW);
+  SET_LDAC1_LOW;
   delayMicroseconds(2);
-  digitalWrite(LDAC1_PIN, HIGH);
+  SET_LDAC1_HIGH;
   delayMicroseconds(2);
 }
 
@@ -369,20 +429,20 @@ void outCVs_3_4()
   CV3Low =  CV3Val & 0xff;
   CV4High = CV4Val >> 8 | 0x90;
   CV4Low =  CV4Val & 0xff;
-  digitalWrite(SS_PIN, LOW);
-  SPI.transfer(CV3High);   // H0x30=OUTA/1x
+  SET_SS_LOW;
+  SPI.transfer(CV3High);   // 0x30=OUTA/1x
   SPI.transfer(CV3Low);
-  digitalWrite(SS_PIN, HIGH);
+  SET_SS_HIGH;
   delayMicroseconds(2);
-  digitalWrite(SS_PIN, LOW);
-  SPI.transfer(CV4High);   // H0x30=OUTA/1x
+  SET_SS_LOW;
+  SPI.transfer(CV4High);   // 0x30=OUTA/1x
   SPI.transfer(CV4Low);
-  digitalWrite(SS_PIN, HIGH);
+  SET_SS_HIGH;
   delayMicroseconds(2);
-  digitalWrite(LDAC2_PIN, LOW);
+  SET_LDAC2_LOW;
   delayMicroseconds(2);
-  digitalWrite(LDAC2_PIN, HIGH);
-  delayMicroseconds(2);  
+  SET_LDAC2_HIGH;
+  delayMicroseconds(2);
 }
 
 // All 4 DACs need to be written, since the two DAC parts share one single SPI SS line
